740d6d0f1a0046dbf479371c34a28f244c8ce463
Naomi Todd <naomiunatodd@gmail.com>
2020-07-22 14:34:52 +0100

    OOP
    Practice Abstract Classes and Interfaces

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Grades
 {
-    public class GradeBook
+    public class GradeBook : GradeTracker
     {
         public GradeBook()
         {
@@ -15,8 +16,10 @@ namespace Grades
             grades = new List<float>();
         }
 
-        public GradeStatistics ComputeStatistics()
+        public override GradeStatistics ComputeStatistics()
         {
+            Console.WriteLine("GradeBook::ComputeStatistuics");
+
             GradeStatistics stats = new GradeStatistics();
 
             float sum = 0;
@@ -33,7 +36,7 @@ namespace Grades
             return stats;
         }
 
-        public void WriteGrades(TextWriter destination)
+        public override void WriteGrades(TextWriter destination)
         {
             for (int i = grades.Count; i > 0; i--)
             {
@@ -41,42 +44,16 @@ namespace Grades
             }
         }
 
-        public void AddGrade(float grade)
+        public override void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        public string Name
+        public override IEnumerator GetEnumerator()
         {
-            get
-            {
-                return _name;
-            }
-
-            set
-            {
-
-                if (string.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Name cannot be null or empty");
-                }
-
-                if (_name != value && NameChanged != null)
-                {
-                    NameChangedEventArgs args = new NameChangedEventArgs();
-                    args.ExistingName = _name;
-                    args.NewName = value;
-
-                    NameChanged(this, args);
-                }
-                _name = value;
-
-            }
+            return grades.GetEnumerator();
         }
 
-        public event NameChangedDelegate NameChanged;
-
-        private string _name;
-        private List<float> grades;
+        protected List<float> grades;
     }
 }
---
+++ Grades/IGradeTracker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.IO;
+
+namespace Grades
+{
+    internal interface IGradeTracker : IEnumerable
+    {
+        void AddGrade(float Grade);
+        GradeStatistics ComputeStatistics();
+        void WriteGrades(TextWriter destination);
+        string Name { get; set; }
+    }
+}
